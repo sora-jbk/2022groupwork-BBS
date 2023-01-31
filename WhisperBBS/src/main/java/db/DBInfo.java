60ab58d2162b83fafc8cae12780665a8530344dc
jibiki <aj22304027@ga.ttc.ac.jp>
2023-01-31 13:51:21 +0900

    DBInfoを改善中

--- WhisperBBS/src/main/java/db/DBInfo.java
+++ WhisperBBS/src/main/java/db/DBInfo.java
@@ -1,9 +1,33 @@
 package db;
 
-public interface DBInfo {
-	String TableName = "POST";
-	String ClassName = "oracle.jdbc.driver.OracleDriver";
-	String ConStr = "jdbc:oracle:thin:@localhost:1521:orcl";
-	String User = "info";
-	String Pass = "pro";
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public abstract class DBInfo {
+	public static final String ClassName;
+	public static final String ConStr;
+	public static final String User;
+	public static final String Pass;
+	
+	static{
+	    Properties properties = new Properties();
+	    String strpass = "/WhisperBBS/properties/db.properties";    
+	    try {
+	        InputStream istream = new FileInputStream(strpass);
+	        properties.load(istream);
+				User = properties.getProperty("Id");
+				Pass = properties.getProperty("Pass");
+				ClassName = properties.getProperty("Class_Name");
+				ConStr = properties.getProperty("Connection_String");
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    
+	    System.out.println(ClassName);
+	    System.out.println(ConStr);
+	    System.out.println(User);
+	    System.out.println(Pass);
+	}
 }
