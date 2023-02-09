254a154bce84549f691f6870224c29f875d905f1
jibiki <aj22304027@ga.ttc.ac.jp>
2023-02-09 14:04:12 +0900

    SQLファイルを変更

--- WhisperBBS/sql/Create.sql
+++ WhisperBBS/sql/Create.sql
@@ -1,35 +1,10 @@
--- whisperユーザーへの切り替え --
-CONN whisper/bbs
-
--- 初期化、作り直し用の削除 --
-DROP TABLE POST;
-DROP SEQUENCE POST_CNT;
-
 -- systemユーザーへの切り替え --
 CONN system / orcl
-
--- 初期化、作り直し用の削除 --
-DROP USER whisper CASCADE;
-DROP TABLESPACE cosmos INCLUDING CONTENTS;
-
--- 表領域の作成 --
-CREATE TABLESPACE cosmos
--- ファイルを置くパスと領域の許容量の指定 --
-DATAFILE 'C:\pleiades\2022-12\workspace\2022groupwork-BBS\WhisperBBS\CreateTableSpace.dbf' 
-SIZE 40M reuse;
-
-
 
 -- ユーザーの作成 --
 CREATE USER whisper
 -- パスワード作成 --
 IDENTIFIED BY bbs
--- 表領域の使用場所 --
-DEFAULT TABLESPACE cosmos
--- 一時表領域の使用場所 --
-TEMPORARY TABLESPACE temp
--- 表領域の割り当て制限 --
-QUOTA 40M on cosmos;
 
 -- 権限の付与 --
 GRANT CREATE SESSION,
@@ -72,14 +47,3 @@ CREATE TABLE POST(
 	CONSTRAINT CK_POST_DELETEFLAG CHECK(DELETED = 0 OR DELETED = 1)
 );
 
-
-
-
-
-
---削除--
-DROP TABLESPACE cosmos INCLUDING CONTENTS AND DATAFILES;
-
-ALTER TABLESPACE cosmos OFFLINE;
-
-SHUTDOWN IMMEDIATE;
